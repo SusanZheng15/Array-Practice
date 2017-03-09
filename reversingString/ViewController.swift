e408e7f172e2155b64d5b15d5659ad9832339bdd
SusanZheng15 <zheng.susan12@gmail.com>
2017-03-08 23:48:01 -0500

    first commit

---
+++ reversingString/ViewController.swift
@@ -0,0 +1,51 @@
+//
+//  ViewController.swift
+//  reversingString
+//
+//  Created by Flatiron School on 3/8/17.
+//  Copyright © 2017 Susan Zheng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController
+{
+    
+    var oldString = "Programming is so much fun!"
+    var reversedString = ""
+
+    override func viewDidLoad()
+    {
+        
+        super.viewDidLoad()
+        
+        reverseAString(oneString: oldString)
+    }
+
+    func reverseAString(oneString: String)
+    {
+        for character in oldString.characters
+        {
+            var char = "\(character)"
+            
+            reversedString = char + reversedString
+        }
+        print(reversedString)
+        
+    }
+   /*
+    let aString = "anutforajaroftuna"
+    
+    var reverse = ""
+    
+    for character in aString.characters {
+    var char = "\(character)"
+    reverse = char + reverse
+    }
+    
+    print(aString == reverse)
+ */
+    
+
+}
+
