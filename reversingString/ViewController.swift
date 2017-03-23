ef89e504ce6d534454c64c752a00dc7ccf781d05
SusanZheng15 <zheng.susan12@gmail.com>
2017-03-22 23:08:19 -0400

    commiting coding challenging

--- reversingString/ViewController.swift
+++ reversingString/ViewController.swift
@@ -33,19 +33,7 @@ class ViewController: UIViewController
         print(reversedString)
         
     }
-   /*
-    let aString = "anutforajaroftuna"
-    
-    var reverse = ""
-    
-    for character in aString.characters {
-    var char = "\(character)"
-    reverse = char + reverse
-    }
-    
-    print(aString == reverse)
- */
-    
+
 
 }
 
